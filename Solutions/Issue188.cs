323beedd7e16f4e039b5f275b1b3952c9d581628
Leon Verschuren <leon.verschuren@outlook.com>
2021-04-04 19:15:32 +0200

    Alternative solution for 189 without RegEx

--- Solutions/Issue188.cs
+++ Solutions/Issue188.cs
@@ -4,19 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RendezvousWithCassidooChallenges
 {
+    /// <summary>
+    /// You’re given two integer arrays (n and m), and an integer k. Using the digits from n and m, return the largest number you can of length k.
+    ///
+    /// Example:
+    /// n = [3,4,6,5]
+    /// m = [9,0,2,5,8,3]
+    /// k = 5
+    /// $ maxNum(n, m, k)
+    /// $ 98655
+    /// </summary>
     [TestClass]
     public class Issue188
     {
-        /// <summary>
-        /// You’re given two integer arrays (n and m), and an integer k. Using the digits from n and m, return the largest number you can of length k.
-        ///
-        /// Example:
-        /// n = [3,4,6,5]
-        /// m = [9,0,2,5,8,3]
-        /// k = 5
-        /// $ maxNum(n, m, k)
-        /// $ 98655
-        /// </summary>
         [TestMethod]
         public void Test()
         {
--- Solutions/Issue189.cs
+++ Solutions/Issue189.cs
@@ -1,31 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RendezvousWithCassidooChallenges
 {
+    /// <summary>
+    /// Given a string, return true if the string represents a valid number. A valid number can include integers, a ., -, or +.
+    /// </summary>
     [TestClass]
     public class Issue189
     {
-        /// <summary>
-        /// Given a string, return true if the string represents a valid number. A valid number can include integers, a ., -, or +.
-        /// </summary>
         [TestMethod]
-        [DataRow("7", true)]
-        [DataRow("0011", true)]
-        [DataRow("+3.14", true)]
-        [DataRow("4.", true)]
-        [DataRow("-.9", true)]
-        [DataRow("-123.456", true)]
-        [DataRow("-0.1", true)]
-        [DataRow("abc", false)]
-        [DataRow("1a", false)]
-        [DataRow("e8", false)]
-        [DataRow("--6", false)]
-        [DataRow("-+3", false)]
-        [DataRow("95x54e53.", false)]
-        [DataRow("95x54e53.", false)]
-        [DataRow("-", false)]
-        [DataRow("..3", false)]
+        [DynamicData(nameof(GetTestCases), DynamicDataSourceType.Method)]
         public void Test(string input, bool isNumber)
         {
             // Arrange
@@ -41,5 +28,107 @@ namespace RendezvousWithCassidooChallenges
         {
             return Regex.IsMatch(input, @"^[+-]{0,1}([0-9]+|([0-9]*\.[0-9]*))$");
         }
+
+        [TestMethod]
+        [DynamicData(nameof(GetTestCases), DynamicDataSourceType.Method)]
+        public void TestAlt(string input, bool isNumber)
+        {
+            // Arrange
+
+            // Act
+            bool result = Parser.IsNumber(input);
+
+            // Assert
+            Assert.AreEqual(isNumber, result);
+        }
+
+        private static IEnumerable<object[]> GetTestCases()
+        {
+            return new[]
+            {
+                new object[] { "7", true },
+                new object[] { "0011", true },
+                new object[] { "+3.14", true },
+                new object[] { "4.", true },
+                new object[] { "-.9", true },
+                new object[] { "-123.456", true },
+                new object[] { "-0.1", true },
+                new object[] { "abc", false },
+                new object[] { "1a", false },
+                new object[] { "e8", false },
+                new object[] { "--6", false },
+                new object[] { "-+3", false },
+                new object[] { "95x54e53.", false },
+                new object[] { "-", false },
+                new object[] { "..3", false }
+            };
+        }
+    }
+
+    public class Parser
+    {
+        private static readonly IEnumerable<char> Modifiers = new[] { '+', '-' };
+        private static readonly IEnumerable<char> Integers = Enumerable.Range(0, 10).SelectMany(r => r.ToString().ToCharArray());
+
+        private readonly IEnumerable<char> _input;
+        private bool _hasModifier;
+        private bool _hasPeriod;
+        private bool _hasNumbers;
+
+        private Parser(string input)
+        {
+            _input = input.ToCharArray();
+        }
+
+        private bool IsNumber()
+        {
+            foreach (char c in _input)
+            {
+                if (c.In(Modifiers))
+                {
+                    if (_hasModifier)
+                    {
+                        // Multiple modifiers in input
+                        return false;
+                    }
+
+                    if (_hasNumbers)
+                    {
+                        // Modifier is proceeded by number
+                        return false;
+                    }
+
+                    _hasModifier = true;
+                }
+                else if (c == '.')
+                {
+                    if (_hasPeriod)
+                    {
+                        // Multiple periods in input
+                        return false;
+                    }
+
+                    _hasPeriod = true;
+                }
+                else if (c.NotIn(Integers))
+                {
+                    return false;
+                }
+                else
+                {
+                    _hasNumbers = true;
+                }
+            }
+
+            return _hasNumbers;
+        }
+
+        public static bool IsNumber(string input) => new Parser(input).IsNumber();
+    }
+
+    public static class EnumerableExtension
+    {
+        public static bool In<T>(this T value, IEnumerable<T> source) => source.Contains(value);
+        public static bool NotIn<T>(this T value, IEnumerable<T> source) => !value.In(source);
     }
 }
